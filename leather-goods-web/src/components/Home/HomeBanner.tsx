e92c667c0fd35e8440975cf0e661de932c1fa2f6
hungmike <maingochungnuce@gmail.com>
2023-09-25 23:49:42 +0700

    complete home-banner interface

---
+++ leather-goods-web/src/components/Home/HomeBanner.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./HomeBanner.module.scss"
+
+const HomeBanner = () => {
+  return (
+    <div id={styles["banner"]}>
+      <div className={styles["banner-image"]}>
+        <Link href="/">
+          <img src="banner.jpg" alt="banner" />
+        </Link>
+        <a href="https://www.facebook.com/ludhandmadevn" target="_blank">
+          <button className={styles["follow"]}>Theo dõi</button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default HomeBanner;
--- leather-goods-web/src/pages/index.tsx
+++ leather-goods-web/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import Header from "@components/Header";
+import HomeBanner from "@components/Home/HomeBanner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function Home() {
       </Head>
       <main>
         <Header />
+        <HomeBanner/>
       </main>
     </>
   );
