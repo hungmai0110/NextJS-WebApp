56b76ac7ec9cc45a0b93af9d7893b8413402f614
hungmike <maingochungnuce@gmail.com>
2023-09-28 15:05:14 +0700

    complete back to top button

---
+++ leather-goods-web/src/components/BackToTopButton.tsx
@@ -0,0 +1,43 @@
+// components/BackToTopButton.js
+import { useState, useEffect } from "react";
+import styles from "@styles/BackToTopButton.module.scss";
+
+const BackToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    const yOffset = window.scrollY;
+    console.log(yOffset);
+    if (yOffset > 250) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return isVisible ? (
+    <button className={styles["btn-top"]} onClick={scrollToTop}>
+      <span>
+        <i className="fa-solid fa-angles-up"></i>
+      </span>
+    </button>
+  ) : (
+    ""
+  );
+};
+
+export default BackToTopButton;
