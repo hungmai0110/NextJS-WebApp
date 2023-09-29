11fa4235e6c65909853c611fb2c75c646153d1fb
hungmike <maingochungnuce@gmail.com>
2023-09-29 11:06:16 +0700

    add hotline button

---
+++ leather-goods-web/src/components/HotlineButton.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Link from "next/link";
+import styles from "@styles/HotlineButton.module.scss";
+
+const HotlineButton = () => {
+  return (
+    <div className={styles["hotline-phone-ring-wrap"]}>
+      <div className={styles["hotline-phone-ring"]}>
+        <div className={styles["hotline-phone-ring-circle"]}></div>
+        <div className={styles["hotline-phone-ring-circle-fill"]}></div>
+        <div className={styles["hotline-phone-ring-img-circle"]}>
+          <Link href="tel:0966516228" className={styles["pps-btn-img"]}>
+            <img
+              src="https://nguyenhung.net/wp-content/uploads/2019/05/icon-call-nh.png"
+              alt="hotline"
+              width="50"
+            />
+          </Link>
+        </div>
+      </div>
+      <div className={styles["hotline-bar"]}>
+        <Link href="tel:0966516228">
+          <span className="text-hotline">0966.516.228</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HotlineButton;
--- leather-goods-web/src/components/Layout.jsx
+++ leather-goods-web/src/components/Layout.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Header from "./Header";
 import BackToTopButton from "@components/BackToTopButton";
 import Footer from "./Footer";
+import HotlineButton from "./HotlineButton";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
       <main>{children}</main>
-      <Footer/>
+      <Footer />
+      <HotlineButton />
       <BackToTopButton />
     </>
   );
