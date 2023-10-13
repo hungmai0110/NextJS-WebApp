b7f70dd12bea4ea45a5ad0dc55f2d64c2854b3db
hungmike <maingochungnuce@gmail.com>
2023-10-13 21:25:51 +0700

    add address options on payment page

--- leather-goods-web/src/pages/payment.tsx
+++ leather-goods-web/src/pages/payment.tsx
@@ -1,87 +1,224 @@
 import styles from "@styles/pages/PaymentPage.module.scss";
 import { formatMoney } from "@utils/utils";
+import axios from "axios";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { CartItem } from "src/interfaces/cartItem.interface";
 
+interface City {
+  Id: string;
+  Name: string;
+  Districts: District[];
+}
+
+interface District {
+  Id: string;
+  Name: string;
+  Wards: Ward[];
+}
+
+interface Ward {
+  Id: string;
+  Name: string;
+}
+
 const PaymentPage = () => {
   const cartItems = useSelector((state: any) => state.cart.cartItems);
+
+  const [cities, setCities] = useState<City[]>([]);
+  const [districts, setDistricts] = useState<District[]>([]);
+  const [wards, setWards] = useState<Ward[]>([]);
+
   const totalMoneyProductsCart = cartItems.reduce(
     (total: number, p: CartItem) => {
-      const itemQuantity = p.quantity ?? 0; 
+      const itemQuantity = p.quantity ?? 0;
       return total + p.promo_price * itemQuantity;
     },
     0
   );
 
+  const renderCities = (data: any) => {
+    setCities(data);
+  };
+
+  const handleCityChange = (event: any) => {
+    const selectedCityId = event.target.value;
+    const selectedCity = cities.find((city) => city.Id === selectedCityId);
+
+    if (selectedCity) {
+      setDistricts(selectedCity.Districts);
+      setWards([]);
+    }
+  };
+
+  const handleDistrictChange = (event: any) => {
+    const selectedDistrictId = event.target.value;
+    const selectedDistrict = districts.find(
+      (district) => district.Id === selectedDistrictId
+    );
+
+    if (selectedDistrict) {
+      setWards(selectedDistrict.Wards);
+    }
+  };
+
+  const validationSchema = Yup.object().shape({
+    fullName: Yup.string().required("Họ và tên không được bỏ trống"),
+    email: Yup.string()
+      .email("Email không hợp lệ")
+      .required("Email không được bỏ trống"),
+    phone: Yup.string().required("Số điện thoại không được bỏ trống"),
+    city: Yup.string().required("Chọn tỉnh thành"),
+    district: Yup.string().required("Chọn quận huyện"),
+    ward: Yup.string().required("Chọn phường xã"),
+    address: Yup.string().required("Địa chỉ không được bỏ trống"),
+    note: Yup.string(),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      fullName: "",
+      email: "",
+      phone: "",
+      city: "",
+      district: "",
+      ward: "",
+      address: "",
+      note: "",
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log('Form submitted with values:', values);
+      alert("Bạn đã đặt đơn hàng thành công!");
+    },
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://raw.githubusercontent.com/kenzouno1/DiaGioiHanhChinhVN/master/data.json"
+        );
+        renderCities(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="container">
       <div className={styles["payment-page"]}>
         <div className={styles["infor-delivery"]}>
-          <h5>THÔNG TIN GIAO HÀNG</h5>
-          <span>
-            Bạn đã có tài khoản chưa?
-            <p className="d-inline-block">Đăng nhập</p>
-          </span>
-          <ul className={styles["form-fill"]}>
-            <li>
-              <input type="text" placeholder="Họ và tên" />
-            </li>
-            <li className="d-flex justify-content-between">
-              <input
-                type="email"
-                className={styles["email"]}
-                placeholder="Email"
-              />
-              <input
-                type="text"
-                className={styles["phone"]}
-                placeholder="Số điện thoại"
-              />
-            </li>
-            <li>
-              <select
-                className="form-select form-select-sm mb-3"
-                id="city"
-                aria-label=".form-select-sm"
-              >
-                <option value="1">Chọn tỉnh thành</option>
-              </select>
+          <form onSubmit={formik.handleSubmit}>
+            <h5>THÔNG TIN GIAO HÀNG</h5>
+            <span>
+              Bạn đã có tài khoản chưa?
+              <p className="d-inline-block">Đăng nhập</p>
+            </span>
+            <ul className={styles["form-fill"]}>
+              <li>
+                <input
+                  type="text"
+                  name="fullName"
+                  placeholder="Họ và tên"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.fullName}
+                />
+              </li>
+              <li className="d-flex justify-content-between">
+                <input
+                  type="email"
+                  className={styles["email"]}
+                  placeholder="Email"
+                />
+                <input
+                  type="text"
+                  className={styles["phone"]}
+                  placeholder="Số điện thoại"
+                />
+              </li>
+              <li>
+                <select
+                  className="form-select form-select-sm mb-3"
+                  id="city"
+                  aria-label=".form-select-sm"
+                  onChange={(e) => {
+                    formik.handleChange(e);
+                    handleCityChange(e);
+                  }}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.city}
+                >
+                  <option value="" selected>
+                    Chọn tỉnh thành
+                  </option>
+                  {cities.map((city) => (
+                    <option key={city.Id} value={city.Id}>
+                      {city.Name}
+                    </option>
+                  ))}{" "}
+                </select>
 
-              <select
-                className="form-select form-select-sm mb-3"
-                id="district"
-                aria-label=".form-select-sm"
-              >
-                <option value="1">Chọn quận huyện</option>
-              </select>
+                <select
+                  className="form-select form-select-sm mb-3"
+                  id="district"
+                  aria-label=".form-select-sm"
+                  onChange={handleDistrictChange}
+                >
+                  <option value="2" selected>
+                    Chọn quận huyện
+                  </option>
+                  {districts.map((district) => (
+                    <option key={district.Id} value={district.Id}>
+                      {district.Name}
+                    </option>
+                  ))}{" "}
+                </select>
 
-              <select
-                className="form-select form-select-sm"
-                id="ward"
-                aria-label=".form-select-sm"
+                <select
+                  className="form-select form-select-sm"
+                  id="ward"
+                  aria-label=".form-select-sm"
+                >
+                  <option value="3" selected>
+                    Chọn phường xã
+                  </option>
+                  {wards.map((ward) => (
+                    <option key={ward.Id} value={ward.Id}>
+                      {ward.Name}
+                    </option>
+                  ))}
+                </select>
+              </li>
+              <li>
+                <input type="text" placeholder="Địa chỉ chi tiết" />
+              </li>
+              <li>
+                <textarea placeholder="Ghi chú: thời gian hay chỉ dẫn địa điểm giao hàng chi tiết hơn."></textarea>
+              </li>
+            </ul>
+            <div className="d-flex justify-content-between align-items-center">
+              <Link href="/products">
+                <span>
+                  <i className="fa-solid fa-rotate-left me-2"></i>Tiếp tục mua
+                  hàng
+                </span>
+              </Link>
+              <button
+                type="submit"
+                className={`${styles["pay-now"]} btn-black`}
               >
-                <option value="1">Chọn phường xã</option>
-              </select>
-            </li>
-            <li>
-              <input type="email" placeholder="Địa chỉ chi tiết" />
-            </li>
-            <li>
-              <textarea placeholder="Ghi chú: thời gian hay chỉ dẫn địa điểm giao hàng chi tiết hơn."></textarea>
-            </li>
-          </ul>
-          <div className="d-flex justify-content-between align-items-center">
-            <Link href="/products">
-              <span>
-                <i className="fa-solid fa-rotate-left me-2"></i>Tiếp tục mua
-                hàng
-              </span>
-            </Link>
-            <button className={`${styles["pay-now"]} btn-black`}>
-              ĐẶT HÀNG
-            </button>
-          </div>
+                ĐẶT HÀNG
+              </button>
+            </div>
+          </form>
         </div>
         <div className={styles["order-detail"]}>
           <h5 className="text-center">ĐƠN HÀNG CỦA BẠN</h5>
